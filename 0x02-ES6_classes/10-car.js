63ac431d368f344685f3a04e931bcc46c9c7b524
Kim_Farida <farimomoh@gmail.com>
2023-01-27 02:23:36 +0000

    Fix: Eslint

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
-  
+
   cloneCar() {
     return new this.constructor(this._brand, this._motor, this._color);
   }
