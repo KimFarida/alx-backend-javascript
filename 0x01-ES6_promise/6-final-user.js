a474060c17b8ade796bc1a999abfabf19faeeb51
Kim_Farida <farimomoh@gmail.com>
2023-01-27 23:12:52 +0000

    Fix: Semi-colon to Array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export function handleProfileSignup(firstName, lastName, fileName) {
-  const promise = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.all(promise)
-    .then(values => {
-      return values.map(value => {
-        return {
-          status: 'fulfilled',
-          value: value
-        }
-      })
-    })
-    .catch(errors => {
-      return errors.map(error => {
-        return {
-          status: 'rejected',
-          value: error
-        }
-      })
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
     });
-  }
+    return result;
+  });
+}
