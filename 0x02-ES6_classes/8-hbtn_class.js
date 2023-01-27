c83241b7e2747c5af2c511f40c17050bd7806861
Kim_Farida <farimomoh@gmail.com>
2023-01-27 02:33:30 +0000

    Fix: Eslint

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-underscore-dangle */
 export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
-  
+
   valueOf() {
     return this._size;
   }
-  
+
   toString() {
     return this._location;
   }
