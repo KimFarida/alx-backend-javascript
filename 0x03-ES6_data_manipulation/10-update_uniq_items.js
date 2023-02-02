438349b8fb857acfc2c1876286ca5614e677753a
Kim_Farida <farimomoh@gmail.com>
2023-02-02 00:04:19 +0000

    Fix: Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
   if (!map || !(map instanceof Map)) throw new Error('Cannot process');
-  for (const [key, value] of map) if (value === 1) map.set(value, 100);
+  for (const [key, value] of map) if (value === 1) map.set(key, 100);
   return map;
 }
